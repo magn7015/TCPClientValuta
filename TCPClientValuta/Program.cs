d8105b908c4fc96c88a25b8e8e117cdc22f8f872
magn7015 <magn7015@edu.easj.dk>
2020-06-15 22:40:56 +0200

    Add project files.

---
+++ TCPClientValuta/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+namespace TCPClientValuta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+            TcpClient clientSocket = new TcpClient("192.168.191.33", 7777);
+
+            Stream ns = clientSocket.GetStream();  //provides a Stream
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+            sw.AutoFlush = true; // enable automatic flushing
+
+            string message = Console.ReadLine();
+            sw.WriteLine(message);
+            string serverAnswer = sr.ReadLine();
+            Console.WriteLine("Server: " + serverAnswer);
+            Console.ReadLine();
+
+            ns.Close();
+
+            clientSocket.Close();
+        }
+    }
+}
